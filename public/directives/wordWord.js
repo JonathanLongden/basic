b7f77a74abf13128962ed4b62f7c6f4b1376de2e
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-01 14:52:58 -0600

    added ico

---
+++ public/directives/wordWord.js
@@ -0,0 +1,48 @@
+// angular.module("garageApp").directive("loginDirective", function() {
+
+//     return {
+//         templateUrl: './../templates/login.html',
+//         restrict: "E"
+//     };
+
+
+
+
+// })
+
+// .directive("signupDirective", function() {
+
+
+//     return {
+//         templateUrl: './../templates/signup.html',
+//         restrict: "E"
+//     }
+
+// })
+
+// // .directive("enterDirective", function(){
+// //
+// //   return {
+// //     templateUrl: './../templates/enter.html',
+// //     restrict: "E"
+// //   }
+// //
+// // })
+
+// .directive("createSaleDirective", function() {
+
+//     return {
+//         templateUrl: './../templates/createSale.html',
+//         restrict: "E"
+//     }
+
+// })
+
+// .directive("contentDirective", function() {
+
+//     return {
+//         templateUrl: './../template/saleContent.html',
+//         restrict: "E"
+//     }
+
+// })
