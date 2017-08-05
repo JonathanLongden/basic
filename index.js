25e06592654e945badf8709484264de7ecad7435
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-05 01:21:17 -0600

    update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.urlencoded());
-app.use(favicon(path.join(__dirname, 'public', '/favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
     // app.use(express.static(__dirname + '/public'));
 app.get('/favicon.ico', function(req, res) {
     res.status(204);
