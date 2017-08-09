b1cb6c553eeb732adcf56576a15ed3692da51fb9
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-09 16:44:16 -0600

    mainCtrl

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -2,45 +2,54 @@ angular.module("basic")
     .controller("mainCtrl", function($scope, $location, mainServ) {
 
 
-        // (function() {
-        //     mainServ.getKnownUser()
-        //         .then(function(response) {
-        //             console.log(response);
-        //             var verify = response.data;
-        //             console.log($scope.userId);
-        //             if (verify.local) {
-        //                 $scope.userId = response.data._id;
-        //             } else {
-        //                 $location.path('/landingPage');
-        //                 //$location.url('/landingPage');
-        //             }
-
-        //         });
-        // })()
+            $scope.names = {};
 
 
-        (function() { //self-invoking function that responses to whether a user is log in or not
-            mainServ.getUpdateUserID()
-                .then(function(response) {
-                    console.log(response);
-                    var userID = response.data;
-                    var user;
-                    if (userID.facebook) {
-                        console.log(userID);
-                    } else if (userID.local) {
-                        console.log(userID);
-                    } else {
-                        $location.path('/landingPage');
-                        // logoutIcon = false;
-                    }
-                    $scope.logOutStuff = logoutIcon;
-                });
-        })()
+            // $scope.getUsername = function(name) {
+            //     $scope.userName = name;
+            // }
+            $scope.userId;
+            (function(userId) {
+                mainServ.getKnownUser(userId)
+                    .then(function(response) {
+                        console.log(response);
+                        var verify = response.data;
+                        console.log(verify);
+                        console.log($scope.userId);
+                        if (verify.local) {
+                            $scope.userId = response.data._id;
+                        } else if (verify.facebook) {
+                            $scope.userId = response.data._id;
+                        } else {
+                            //$location.url('/landingPage');
+                            $location.path('/landingPage');
+                        }
 
-        $scope.signOut = function() { //logs a you out and redirects them to the home page
-            // console.log("SignOut");
-            mainServ.getSignOut()
-                .then(function(response) {})
-        }
+                    });
 
-    });
+
+                // (function() { //self-invoking function that responses to whether a user is log in or not
+                //     mainServ.getKnownUser()
+                //         .then(function(response) {
+                //             console.log(response);
+                //             var userID = response.data;
+                //             var user;
+                //             if (userID.facebook) {
+                //                 console.log(userID);
+                //             } else if (userID.local) {
+                //                 console.log(userID);
+                //             } else {
+                //                 $location.path('/landingPage');
+                //                 // logoutIcon = false;
+                //             }
+                //             $scope.logOutStuff = logoutIcon;
+                //         });
+                // })()
+
+                $scope.signOut = function() { //logs a you out and redirects them to the home page
+                    // console.log("SignOut");
+                    mainServ.getSignOut()
+                        .then(function(response) {})
+                }
+
+            });
