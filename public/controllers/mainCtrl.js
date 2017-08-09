318be4c49adb2c25512fcfb3b312f54535d6dc3e
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-09 17:11:06 -0600

    test

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -18,26 +18,26 @@ angular.module("basic")
                 .then(function(response) {})
         }
 
-        // (function(userId) { //self-invoking function that responses to whether a user is log in or not
-        //     mainServ.getKnownUser(userId)
-        //         .then(function(response) {
-        //             console.log(response);
-        //             var userID = response.data;
-        //             var user;
-        //             console.log(userID);
-        //             console.log($scope.userId)
-        //             console.log($userd)
-        //             if (userID.facebook) {
-        //                 console.log(userID);
-        //             } else if (userID.local) {
-        //                 console.log(userID);
-        //             } else {
-        //                 $location.path('/landingPage');
-        //                 // logoutIcon = false;
-        //             }
-        //             $scope.logOutStuff = logoutIcon;
-        //         });
-        // })();
+        (function(userId) { //self-invoking function that responses to whether a user is log in or not
+            mainServ.getKnownUser(userId)
+                .then(function(response) {
+                    console.log(response);
+                    var userID = response.data;
+                    var user;
+                    console.log(userID);
+                    console.log($scope.userId)
+                    console.log($userd)
+                    if (userID.facebook) {
+                        console.log(userID);
+                    } else if (userID.local) {
+                        console.log(userID);
+                    } else {
+                        $location.path('/landingPage');
+                        // logoutIcon = false;
+                    }
+                    $scope.logOutStuff = logoutIcon;
+                });
+        })();
 
 
 
