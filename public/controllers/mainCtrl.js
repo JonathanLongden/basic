3460589e86e927c51b402bfd055d050b3aa14716
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-09 15:38:10 -0600

    mainCtrl

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,100 +1,39 @@
 angular.module("basic")
     .controller("mainCtrl", function($scope, $location, mainServ) {
 
-        // (function(userInfo) {
-        //     mainServ.getKnownUser(userInfo)
-        //         .then(function(response) {
-        //             //console.log(response);
-        //             var verify = response.data;
-        //             // console.log($scope.userId);
-        //             if (verify.local) {
-        //                 $scope.userId = response.data._id;
-        //             } else if (verify.facebook) {
-        //                 $scope.userId = response.data._id;
-        //             } else {
-        //                 $location.url('/login');
-        //             }
-
-        //         });
-
-
-        // (function (){                   //self-invoking function that responses to whether a user is log in or not
-        //     mainServ.getKnownUser()
-        //     .then(function(response){
-        //       var userID = response.data;
-        //       var user;
-        //       if (userID.facebook){
-        //         logoutIcon = true;
-        //       } else if (userID.local) {
-        //         logoutIcon = true;
-        //       } else {
-        //         logoutIcon = false;
-        //       }
-        //         $scope.logOutStuff = logoutIcon;
-        //     });
-        //   })()
-
-        this.myDate = new Date();
-        this.isOpen = false;
-
-        $scope.category = {
-            model: null,
-            availableOptions: [
-                { id: '1', name: 'Sample Contaminations/Bias Potential' },
-                { id: '2', name: 'Environment/Field Conditions' },
-                { id: '3', name: 'Training' },
-                { id: '4', name: 'Documentation/Communication' },
-                { id: '5', name: 'Data Validation' },
-                { id: '6', name: 'Data Import' },
-                { id: '7', name: 'Data Storage' },
-                { id: '8', name: 'Data Export/Display' },
-                { id: '9', name: 'Data Evaluations/Calculations' },
-                { id: '10', name: 'Documentation/Communication' },
-                { id: '11', name: 'Design/Calculations' },
-                { id: '12', name: 'Deliverables' }
-            ]
-        };
-
-
-        $scope.formInfo = {};
-        $scope.qaCard = function(mmddyyyy, person, category, description, mitigration) {
-            var qaCardData = {
-                riskObservationDate: mmddyyyy,
-                riskReportedBy: person,
-                riskCategory: category,
-                riskDescription: description,
-                riskMitigration: mitigration
-
-            };
-            console.log(qaCardData);
-            mainServ.qcCardPost(qaCardData)
+        (function(userInfo) {
+            mainServ.getKnownUser(userInfo)
                 .then(function(response) {
-                    //console.log("You have Signed in like a champ!");
-                    var verify = response;
-                    //console.log(response.data)
-                    //  if (verify.user){
-                    //     $location.path('map');
-                    //     $scope.userLogin = "";
-                    //   } else {
-                    //     $scope.wrongCred = false;
-                    //   }
-                })
-        };
-        // (function (){                   //self-invoking function that responses to whether a user is log in or not
+                    //console.log(response);
+                    var verify = response.data;
+                    // console.log($scope.userId);
+                    if (verify.local) {
+                        $scope.userId = response.data._id;
+                    } else if (verify.facebook) {
+                        $scope.userId = response.data._id;
+                    } else {
+                        $location.url('/landingPage');
+                    }
+
+                });
+        })()
+
+
+        // (function() { //self-invoking function that responses to whether a user is log in or not
         //     mainServ.getKnownUser()
-        //     .then(function(response){
-        //         var userID = response.data;
-        //         var user;
-        //         if (userID.facebook){
-        //         logoutIcon = true;
-        //         } else if (userID.local) {
-        //         logoutIcon = true;
-        //         } else {
-        //         logoutIcon = false;
-        //         }
-        //         $scope.logOutStuff = logoutIcon;
-        //     });
-        //     })()
+        //         .then(function(response) {
+        //             var userID = response.data;
+        //             var user;
+        //             if (userID.facebook) {
+        //                 logoutIcon = true;
+        //             } else if (userID.local) {
+        //                 logoutIcon = true;
+        //             } else {
+        //                 logoutIcon = false;
+        //             }
+        //             $scope.logOutStuff = logoutIcon;
+        //         });
+        // })()
 
         $scope.signOut = function() { //logs a you out and redirects them to the home page
             console.log("SignOut");
