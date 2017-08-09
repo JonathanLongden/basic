3439a06e15c503ea730475d7f0aed1e88822fc62
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-09 16:08:40 -0600

    mainCtrl

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -2,41 +2,43 @@ angular.module("basic")
     .controller("mainCtrl", function($scope, $location, mainServ) {
 
 
-        (function() {
-            mainServ.getKnownUser()
-                .then(function(response) {
-                    console.log(response);
-                    var verify = response.data;
-                    console.log($scope.userId);
-                    if (verify.local) {
-                        $scope.userId = response.data._id;
-                    } else {
-                        $location.path('/landingPage');
-                        //$location.url('/landingPage');
-                    }
-
-                });
-        })()
-
-
-        // (function() { //self-invoking function that responses to whether a user is log in or not
+        // (function() {
         //     mainServ.getKnownUser()
         //         .then(function(response) {
-        //             var userID = response.data;
-        //             var user;
-        //             if (userID.facebook) {
-        //                 logoutIcon = true;
-        //             } else if (userID.local) {
-        //                 logoutIcon = true;
+        //             console.log(response);
+        //             var verify = response.data;
+        //             console.log($scope.userId);
+        //             if (verify.local) {
+        //                 $scope.userId = response.data._id;
         //             } else {
-        //                 logoutIcon = false;
+        //                 $location.path('/landingPage');
+        //                 //$location.url('/landingPage');
         //             }
-        //             $scope.logOutStuff = logoutIcon;
+
         //         });
         // })()
 
+
+        (function() { //self-invoking function that responses to whether a user is log in or not
+            mainServ.getKnownUser()
+                .then(function(response) {
+                    console.log(response);
+                    var userID = response.data;
+                    var user;
+                    if (userID.facebook) {
+                        console.log(userID);
+                    } else if (userID.local) {
+                        console.log(userID);
+                    } else {
+                        $location.path('/landingPage');
+                        // logoutIcon = false;
+                    }
+                    $scope.logOutStuff = logoutIcon;
+                });
+        })()
+
         $scope.signOut = function() { //logs a you out and redirects them to the home page
-            console.log("SignOut");
+            // console.log("SignOut");
             mainServ.getSignOut()
                 .then(function(response) {})
         }
