d6b4bea5f98fe3899f9a1c6fec69b90cc510e8a5
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-09 09:15:44 -0600

    style issues

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,9 +1,41 @@
 angular.module("basic")
     .controller("mainCtrl", function($scope, $location, mainServ) {
+
+        // (function(userInfo) {
+        //     mainServ.getKnownUser(userInfo)
+        //         .then(function(response) {
+        //             //console.log(response);
+        //             var verify = response.data;
+        //             // console.log($scope.userId);
+        //             if (verify.local) {
+        //                 $scope.userId = response.data._id;
+        //             } else if (verify.facebook) {
+        //                 $scope.userId = response.data._id;
+        //             } else {
+        //                 $location.url('/login');
+        //             }
+
+        //         });
+
+
+        // (function (){                   //self-invoking function that responses to whether a user is log in or not
+        //     mainServ.getKnownUser()
+        //     .then(function(response){
+        //       var userID = response.data;
+        //       var user;
+        //       if (userID.facebook){
+        //         logoutIcon = true;
+        //       } else if (userID.local) {
+        //         logoutIcon = true;
+        //       } else {
+        //         logoutIcon = false;
+        //       }
+        //         $scope.logOutStuff = logoutIcon;
+        //     });
+        //   })()
+
         this.myDate = new Date();
         this.isOpen = false;
-
-        //$scope.mmddyyyy = myDate;
 
         $scope.category = {
             model: null,
