64d0aa9f6a703dfeb0e02dcf89023b841008b9c4
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-10 15:03:55 -0600

    fixed forms

--- public/controllers/qaCtrl.js
+++ public/controllers/qaCtrl.js
@@ -84,18 +84,17 @@ angular.module('basic').controller('qaCtrl', function($scope, $location, mainSer
 
 
     $scope.formInfo = {};
-    // $scope.qaCard = function(mmddyyyy, person, category, description, mitigration) {
-    //     var qaCardData = {
-    //         riskObservationDate: mmddyyyy,
-    //         riskReportedBy: person,
-    //         riskCategory: category,
-    //         riskDescription: description,
-    //         riskMitigration: mitigration
+    $scope.qaCard = function(mmddyyyy, person, category, description, mitigration) {
+        var qacard = {
+            riskObservationDate: mmddyyyy,
+            riskReportedBy: person,
+            riskCategory: category,
+            riskDescription: description,
+            riskMitigration: mitigration
 
-    //     };
-    //     console.log(qaCardData);
-    $scope.postQaCard = function(sale) {
-        mainServ.qcCardPost(qaCardData)
+        };
+        console.log(qacard);
+        mainServ.qcCardPost(qacard)
             .then(function(response) {
                 console.log("You Created a Card");
                 var verify = response;
@@ -105,49 +104,8 @@ angular.module('basic').controller('qaCtrl', function($scope, $location, mainSer
     this.myDate = new Date();
     this.isOpen = false;
 
-    $scope.category = {
-        model: null,
-        availableOptions: [
-            { id: '1', name: 'Sample Contaminations/Bias Potential' },
-            { id: '2', name: 'Environment/Field Conditions' },
-            { id: '3', name: 'Training' },
-            { id: '4', name: 'Documentation/Communication' },
-            { id: '5', name: 'Data Validation' },
-            { id: '6', name: 'Data Import' },
-            { id: '7', name: 'Data Storage' },
-            { id: '8', name: 'Data Export/Display' },
-            { id: '9', name: 'Data Evaluations/Calculations' },
-            { id: '10', name: 'Documentation/Communication' },
-            { id: '11', name: 'Design/Calculations' },
-            { id: '12', name: 'Deliverables' }
-        ]
-    };
 
 
-    // $scope.formInfo = {};
-    // $scope.qaCard = function(mmddyyyy, person, category, description, mitigration) {
-    //     var qaCardData = {
-    //         riskObservationDate: mmddyyyy,
-    //         riskReportedBy: person,
-    //         riskCategory: category,
-    //         riskDescription: description,
-    //         riskMitigration: mitigration
-
-    //     };
-    //     console.log(qaCardData);
-    //     mainServ.qcCardPost(qaCardData)
-    //         .then(function(response) {
-    //             //console.log("You have Signed in like a champ!");
-    //             var verify = response;
-    //             //console.log(response.data)
-    //             //  if (verify.user){
-    //             //     $location.path('map');
-    //             //     $scope.userLogin = "";
-    //             //   } else {
-    //             //     $scope.wrongCred = false;
-    //             //   }
-    //         })
-    // };
 
     function real(data) {
         var person = data.data.local.userName;
