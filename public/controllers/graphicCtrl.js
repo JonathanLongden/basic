3df8032f398dce720864db6b3070e499bed21b25
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-12 00:05:39 -0600

    test

--- public/controllers/graphicCtrl.js
+++ public/controllers/graphicCtrl.js
@@ -31,7 +31,9 @@ angular.module('basic').controller('graphicCtrl', function($scope, $location, ma
         mainServ.getAllCards()
             .then(function(response) {
                 console.log(response);
-                console.log(response.data.riskCategory);
+                console.log(response.data);
+                //console.log(response.data.riskCategory);
+                //
                 console.log(response.riskCategory);
                 var riskCategory = [];
                 for (var i = 0; i < response.length; i++) {
