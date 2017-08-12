13a22da26de2ec2d9a116944b10adf47a0033ffa
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-12 00:40:16 -0600

    scope object

--- public/controllers/graphicCtrl.js
+++ public/controllers/graphicCtrl.js
@@ -1,17 +1,17 @@
 angular.module('basic').controller('graphicCtrl', function($scope, $location, mainServ, $rootScope) {
 
     //$scope.pieChartConfig.graphData[0].value
-    $scope.pieChartConfig = {
-        graphData: [
-            { name: 'A', value: 50 },
-            { name: 'B', value: 100 },
-            { name: 'C', value: 100 },
-            { name: 'D', value: 35 },
-            { name: 'E', value: 125 },
-            { name: 'F', value: 125 }
-            //{name: 'F', value: 125}
-        ]
-    };
+    // $scope.pieChartConfig = {
+    //     graphData: [
+    //         // { name: 'A', value: 50 },
+    //         // { name: 'B', value: 100 },
+    //         // { name: 'C', value: 100 },
+    //         // { name: 'D', value: 35 },
+    //         // { name: 'E', value: 125 },
+    //         // { name: 'F', value: 125 }
+    //         //{name: 'F', value: 125}
+    //     ]
+    // };
     // $scope.pieChartConfig = {
     //     graphData: [
     //         { name: 'A', value: 50 },
@@ -52,7 +52,8 @@ angular.module('basic').controller('graphicCtrl', function($scope, $location, ma
                         riskCategory.push(category)
                     }
                 }
-
+                //creates & counts items into an object
+                //{deliverables:1}
                 result = {};
                 for (var i = 0; i < riskCategory.length; ++i) {
                     if (!result[riskCategory[i]])
@@ -60,7 +61,8 @@ angular.module('basic').controller('graphicCtrl', function($scope, $location, ma
                     ++result[riskCategory[i]];
                 }
                 console.log(result);
-
+                //should scope this object
+                scope.pieChartConfig.result;
 
             })
     }
