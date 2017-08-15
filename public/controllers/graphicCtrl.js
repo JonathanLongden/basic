52dd10c47529fd8e1e821161710b543f4219e01a
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-15 08:42:10 -0600

    update

--- public/controllers/graphicCtrl.js
+++ public/controllers/graphicCtrl.js
@@ -1,112 +1,113 @@
 angular.module('basic').controller('graphicCtrl', function($scope, $location, mainServ, $rootScope) {
 
     $scope.pieChartConfig;
-    // $scope.pieChartConfig = {
-    //     graphData: [
-    //         { name: 'A', value: 50 },
-    //         { name: 'B', value: 100 },
-    //         { name: 'C', value: 100 },
-    //         { name: 'D', value: 35 },
-    //         { name: 'E', value: 125 },
-    //         { name: 'F', value: 125 },
-    //         { name: 'F', value: 125 }
-    //     ]
-    // };
-    //$scope.pieChartConfig;
-    console.log($scope.pieChartConfig)
-
-    function personsCards(data) {
-        var persondata = data;
-        try {
-            if (persondata) {
-                try {
-                    var riskCategory = [];
-
-                    for (var i = 0; i < persondata.length; i++) {
-                        var category = persondata[i].riskCategory;
-                        if (category != null) {
-                            //if not null added to list
-                            riskCategory.push(category)
-                        }
-                    }
-
-                    var count = {};
-
-                    riskCategory.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
-
-                    var txt = "";
-                    for (x in count) {
-                        txt += x + "," + count[x] + ",";
-
-                    }
-
-                    var datalist = [];
-                    datalist = txt.split(",");
-                    //console.log(datalist);
-                    var len = datalist.length;
-                    // for (var i = 0; i < len; i++) {
-                    //     console.log(datalist[i]);
-                    // }
-                    //Priting out as a list
-                    var graphData = [];
-                    var alldata = new Object();
-                    for (var i = 0; i < len; i++) {
-                        if ((i % 2) == 0) {
-                            if (alldata[i] == "" || datalist[i + 1] == undefined) {
-                                //do nothing
-                            } else {
-                                alldata[i] = ({ name: datalist[i], value: datalist[i + 1] });
-                                graphData.push(alldata[i]);
-                            }
-
-                        }
-                        //console.log(i);
-                    }
-
-                    $scope.pieChartConfig = graphData;
-                    console.log($scope.pieChartConfig);
-                    return graphData;
-                    //return true;
-                } catch (err) {
-                    console.log(err);
-                }
-
-            } else {
-                //$location.path('/landingPage');
-                return null;
-
-            };
-        } catch (err) {
-            //$location.path('/landingPage');
-            console.log(err);
-        }
-
+    $scope.pieChartConfig = {
+        graphData: [
+            { name: 'A', value: 50 },
+            { name: 'B', value: 100 },
+            { name: 'C', value: 100 },
+            { name: 'D', value: 35 },
+            { name: 'E', value: 125 },
+            { name: 'F', value: 125 },
+            { name: 'F', value: 125 }
+        ]
     };
+    //$scope.pieChartConfig;
+    console.log($scope.pieChartConfig);
+    console.log($scope.pieChartConfig.graphData);
+
+    // function personsCards(data) {
+    //     var persondata = data;
+    //     try {
+    //         if (persondata) {
+    //             try {
+    //                 var riskCategory = [];
+
+    //                 for (var i = 0; i < persondata.length; i++) {
+    //                     var category = persondata[i].riskCategory;
+    //                     if (category != null) {
+    //                         //if not null added to list
+    //                         riskCategory.push(category)
+    //                     }
+    //                 }
+
+    //                 var count = {};
+
+    //                 riskCategory.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
+
+    //                 var txt = "";
+    //                 for (x in count) {
+    //                     txt += x + "," + count[x] + ",";
+
+    //                 }
+
+    //                 var datalist = [];
+    //                 datalist = txt.split(",");
+    //                 //console.log(datalist);
+    //                 var len = datalist.length;
+    //                 // for (var i = 0; i < len; i++) {
+    //                 //     console.log(datalist[i]);
+    //                 // }
+    //                 //Priting out as a list
+    //                 var graphData = [];
+    //                 var alldata = new Object();
+    //                 for (var i = 0; i < len; i++) {
+    //                     if ((i % 2) == 0) {
+    //                         if (alldata[i] == "" || datalist[i + 1] == undefined) {
+    //                             //do nothing
+    //                         } else {
+    //                             alldata[i] = ({ name: datalist[i], value: datalist[i + 1] });
+    //                             graphData.push(alldata[i]);
+    //                         }
+
+    //                     }
+    //                     //console.log(i);
+    //                 }
+
+    //                 $scope.pieChartConfig = graphData;
+    //                 console.log($scope.pieChartConfig);
+    //                 return graphData;
+    //                 //return true;
+    //             } catch (err) {
+    //                 console.log(err);
+    //             }
+
+    //         } else {
+    //             //$location.path('/landingPage');
+    //             return null;
+
+    //         };
+    //     } catch (err) {
+    //         //$location.path('/landingPage');
+    //         console.log(err);
+    //     }
+
+    // };
 
 
-    //self-invoking function that responses to whether a user is log in or not
-    (function() {
-        mainServ.getAllCards()
-            .then(function(response) {
-                var data = response;
-                try {
-                    var cards = personsCards(data);
-                    if (cards) {
-                        console.log(cards)
-                        $scope.pieChartConfig = cards;
-                        console.log($scope.pieChartConfig)
-                            //true
-                    } else {
+    // //self-invoking function that responses to whether a user is log in or not
+    // (function() {
+    //     mainServ.getAllCards()
+    //         .then(function(response) {
+    //             var data = response;
+    //             try {
+    //                 var cards = personsCards(data);
+    //                 if (cards) {
+    //                     console.log(cards)
+    //                     $scope.pieChartConfig = cards;
+    //                     console.log($scope.pieChartConfig)
+    //                         //true
+    //                 } else {
 
-                        //false No data
-                    }
-                } catch (err) {
+    //                     //false No data
+    //                 }
+    //             } catch (err) {
 
-                    console.log(err);
-                }
+    //                 console.log(err);
+    //             }
 
-            });
-    })()
+    //         });
+    // })()
 
 
     $scope.getCards = function() {
