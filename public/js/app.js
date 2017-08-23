0dec5224b43d0f9b3bc05add569a8fb7cee66ccf
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-23 16:29:50 -0600

    test

--- public/js/app.js
+++ public/js/app.js
@@ -8,52 +8,52 @@ angular.module("basic", ['ui.router'])
             .state("/", {
                 url: "/home",
                 templateUrl: "./templates/home.html", // or template: //'<h1>Home Page</h1>' 
-                controller: "mainCtrl"
+                controller: "mainCtrl",
             })
             .state("forms", {
                 url: "/forms",
                 templateUrl: "./templates/forms.html",
-                controller: "mainCtrl"
+                controller: "mainCtrl",
             })
             .state("login", {
                 url: "/login",
                 templateUrl: "./templates/login.html",
-                controller: "loginCtrl"
+                controller: "loginCtrl",
             })
             .state("signup", {
                 url: "/signup",
                 templateUrl: "./templates/signup.html",
-                controller: "loginCtrl"
+                controller: "loginCtrl",
             })
             .state("riskcard", {
                 url: "/riskcard",
                 templateUrl: "./templates/riskcard.html",
-                controller: "qaCtrl"
+                controller: "qaCtrl",
             })
             .state("mitigation", {
                 url: "/mitigation",
                 templateUrl: "./templates/mitigation.html",
-                controller: "qaCtrl"
+                controller: "qaCtrl",
             })
             .state("summary", {
                 url: "/summary",
                 templateUrl: "./templates/summary.html",
-                controller: "summaryCtrl"
+                controller: "summaryCtrl",
             })
             .state("admitsummaryreports", {
                 url: "/admitsummaryreports",
                 templateUrl: "./templates/admitsummaryreports.html",
-                controller: "summaryCtrl"
+                controller: "summaryCtrl",
             })
             .state("usersummaryreports", {
                 url: "/usersummaryreports",
                 templateUrl: "./templates/usersummaryreports.html",
-                controller: "summaryCtrl"
+                controller: "summaryCtrl",
             })
             .state("graphic", {
                 url: "/graphic",
                 templateUrl: "./templates/graphic.html",
-                controller: "graphicCtrl.js"
+                controller: "graphicCtrl.js",
             });
 
 
