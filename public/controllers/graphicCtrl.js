c6f5fb199eaafbf15f0d68c14f8cb3a156599e63
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-23 14:16:28 -0600

    update

--- public/controllers/graphicCtrl.js
+++ public/controllers/graphicCtrl.js
@@ -1,104 +1,100 @@
-(function() {
-    'use strict';
-    angular.module('basic').controller('graphicCtrl', [$http, myControllerFunc], function($scope, $location, mainServ, $rootScope) {
-        function myControllerFunc($http) {
-            $scope.MyCards = null;
-            $scope.pieChartConfig = {
-                graphData: []
-            };
+angular.module('basic').controller('graphicCtrl', function($scope, $location, mainServ, $rootScope) {
+    $scope.MyCards = null;
+    $scope.pieChartConfig = {
+        graphData: []
+    };
 
-            function personsCards(data) {
-                var persondata = data;
+    function personsCards(data) {
+        var persondata = data;
+        try {
+            if (persondata) {
                 try {
-                    if (persondata) {
-                        try {
-                            var riskCategory = [];
-                            for (var i = 0; i < persondata.length; i++) {
-                                var category = persondata[i].riskCategory;
-                                if (category != null) {
-                                    //if not null added to list
-                                    riskCategory.push(category)
-                                }
-                            }
-                            var count = {};
-
-                            riskCategory.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
-
-                            var txt = "";
-                            for (var x in count) {
-                                txt += x + "," + count[x] + ",";
-
-                            }
-                            var datalist = [];
-                            datalist = txt.split(",");
-                            var len = datalist.length;
-
-                            var alldata = {};
-                            for (var j = 0; j < len; j++) {
-                                if ((j % 2) == 0) {
-                                    if (alldata[j] == "" || datalist[j + 1] == undefined) {
-                                        //do nothing
-                                    } else {
-                                        alldata[j] = ({ name: datalist[j], value: datalist[j + 1] });
-                                        $scope.pieChartConfig.graphData.push(alldata[j]);
-                                    }
-
-                                }
-
-                            }
-
-                            return true;
-
-                        } catch (err) {
-                            console.log(err);
+                    var riskCategory = [];
+                    for (var i = 0; i < persondata.length; i++) {
+                        var category = persondata[i].riskCategory;
+                        if (category != null) {
+                            //if not null added to list
+                            riskCategory.push(category);
                         }
+                    }
+                    var count = {};
 
-                    } else {
+                    riskCategory.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
 
-                        return null;
+                    var txt = "";
+                    for (var x in count) {
+                        txt += x + "," + count[x] + ",";
 
                     }
-                } catch (err) {
+                    var datalist = [];
+                    datalist = txt.split(",");
+                    var len = datalist.length;
 
+                    var alldata = {};
+                    for (var j = 0; j < len; j++) {
+                        if ((j % 2) == 0) {
+                            if (alldata[j] == "" || datalist[j + 1] == undefined) {
+                                //do nothing
+                            } else {
+                                alldata[j] = ({ name: datalist[j], value: datalist[j + 1] });
+                                $scope.pieChartConfig.graphData.push(alldata[j]);
+                            }
+
+                        }
+
+                    }
+
+                    return true;
+
+                } catch (err) {
                     console.log(err);
                 }
 
+            } else {
+
+                return null;
+
             }
-            //self-invoking function that responses to whether a user is log in or not
-            //(function() {
-            // code
-            //    })()
-            (function() {
-                // $scope.getMyCards = function() {
-                mainServ.getAllCards()
-                    .then(function(response) {
-                        $scope.MyCards = response;
-                        //var data = response;
-                        try {
-                            var cards = personsCards(response);
-                            if (cards) {
-                                //true Display Grapch
-                            } else {
-                                //false Show a Sign?
-                            }
-                        } catch (err) {
-                            console.log(err);
-                        }
+        } catch (err) {
 
-                    });
-            })();
-
-            $scope.deleteCard = function(id) {
-                //console.log(id);
-                mainServ.deleteCard(id)
-                    .then(function(response) {
-                        $scope.getAllCards();
-                        //$scope.getCards();
-                    });
-            };
-
-
+            console.log(err);
         }
 
-    });
-})();
+    }
+    //self-invoking function that responses to whether a user is log in or not
+    //(function() {
+    // code
+    //    })()
+    (function() {
+        // $scope.getMyCards = function() {
+        mainServ.getAllCards()
+            .then(function(response) {
+                $scope.MyCards = response;
+                //var data = response;
+                try {
+                    var cards = personsCards(response);
+                    if (cards) {
+                        //true Display Grapch
+                    } else {
+                        //false Show a Sign?
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+
+            });
+    })();
+
+    $scope.deleteCard = function(id) {
+        //console.log(id);
+        mainServ.deleteCard(id)
+            .then(function(response) {
+                $scope.getAllCards();
+                //$scope.getCards();
+            });
+    };
+
+
+
+
+});
