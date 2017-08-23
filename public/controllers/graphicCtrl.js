2402470560f213e0f986af9a2100a3092e140292
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-08-23 16:38:04 -0600

    test

--- public/controllers/graphicCtrl.js
+++ public/controllers/graphicCtrl.js
@@ -4,7 +4,9 @@ angular.module('basic').controller('graphicCtrl', ['$scope', '$state', '$locatio
         $scope.pieChartConfig = {
             graphData: []
         };
-        $scope.load = $state.reload();
+        $scope.load = function() {
+            $state.reload();
+        };
 
         function personsCards(data) {
             var persondata = data;
